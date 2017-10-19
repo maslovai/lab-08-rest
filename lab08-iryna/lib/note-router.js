2be72dfddd569a77715bd1c8fd15091c540044c0
Iryna Maslova <maslovai@hotmail.com>
2017-10-19 12:44:30 -0700

    tests written but not passing

--- lab08-iryna/lib/note-router.js
+++ lab08-iryna/lib/note-router.js
@@ -4,54 +4,46 @@ const router = require('./router.js');
 
 let notes = [];
 
-let sendStatus = (res, status)=>{
+let sendStatus = (res, status) => {
   res.writeHead(status);
   res.end();
 }
 
-let sendJSON = (res, status, data)=>{
+let sendJSON = (res, status, data) => {
   res.writeHead(status, {"Content-Type":"application/json"});
   res.end(JSON.stringify(data));
 }
 
-
-router.get('/api/notes',(req, res) =>{
+router.get('/api/notes',(req, res) => {
   let id = req.url && req.url.query && req.url.query.id;
   if(id){
-
     let note = notes.filter((note) => note.id === id);
-
     if (note) sendJSON(res, 200, note)
-    else sendStatus(res, 400, "invalid note")
+    else sendStatus(res, 404, "invalid id")
   }
   else{
     let allNotes = {notes:notes};
     sendJSON(res, 200, allNotes)
   }
-
 });
 
 router.post('/api/notes',(req, res) =>{
-//400
-if(!req.body.title) return sendStatus(res, 400, "Missing Title");
-if(!req.body.content) return sendStatus(res, 400, "Missing Content");
-//save the note
-let note = new Note(req.body);
-notes.push(note);
-console.log("all notes:",notes);
-sendJSON(res, 200, note)
+    if (!req.body) return sendStatus(res, 400, "bad request");
+    if(!req.body.title) return sendStatus(res, 400, "Missing Title");
+    if(!req.body.content) return sendStatus(res, 400, "Missing Content");
+    let note = new Note(req.body);
+    notes.push(note);
+    console.log("all notes: ",notes);
+    sendJSON(res, 200, note)
 });
 
 router.put('/api/notes',(req, res) =>{
   if(!req.body.title) return sendStatus(res, 400, "Missing Title");
   if(!req.body.content) return sendStatus(res, 400, "Missing Content");
-
-  //find note to update
   let id = req.url && req.url.query && req.url.query.id;
   if(id){
     let index = notes.indexOf(notes.id);
     notes.splice(index, 1);
-  //save the note
     let notePut = new Note(req.body);
     notes.push(notePut);
     console.log("all notes:",notes);
@@ -74,20 +66,12 @@ router.patch('/api/notes',(req, res) => {
 });
 
 router.delete('/api/notes',(req, res) => {
-//find note to update
     let id = req.url && req.url.query && req.url.query.id;
     if(id){
-      // var index = notes.indexOf(id);
       notes = notes.filter((ele) => { return ele.id !== id });
-      // console.log("index to delete: ", index);
-      // notes.splice(index, 1);
       console.log("all notes: ",notes);
           sendJSON(res, 200, `note ${id} deleted`)
     }else{
       sendStatus(res, 400, 'id not found')
     }
-  // check for id
-  //find id in notes
-  //DELETE
-  //send 200
 })
--- lab08-iryna/test/note-routes.test.js
+++ lab08-iryna/test/note-routes.test.js
@@ -1,24 +1,93 @@
 'use strict';
- const PORT = 8080;
+ const PORT = 8000;
+ const server = require('../lib/server');
  const request = require('superagent');
  const expect = require('expect');
- const host = 'localhost:'+PORT;
+ // const host = 'http://localhost:'+ PORT;
+
  describe('server', function() {
    before(function(done) {
-     server.listen(PORT, done);
+     server.start(PORT, done);
    });
-
    after(function(done) {
-     server.close(done);
+     server.stop(done);
    });
+ });
+
+ describe('post test - 200', function(){
+   it('should respond with the body content for a post request with a valid body',function(done){
+     request
+     .post(`http://localhost:${PORT}/api/notes`)
+     .send({title:"First Note", content:"First text"})
+     .end((err,res) => {
+       expect(err).toBe(null);
+       expect(res.status).toEqual(200);
+       expect(res.body.title).toBe("First Note");
+       expect(res.body.content).toBe("First Text");
+       let idForGet = note.id;
+     })
+   })
+ });
+
+ describe('post test - 400', function(){
+   it('should respond with  "bad request" if no request body',function(done){
+     request
+     .post(`http://localhost:${PORT}/api/notes`)
+     .send({})
+     .end((err,res) => {
+       expect(err).not.toBe(null);
+       expect(res.status).toEqual(400);
+       expect(res.text).toBe("bad request");
+     })
+   })
+ });
+
+ describe('post test - 400', function(){
+ it('should respond with  "bad request" if request body is invalid',function(done){
+   request
+   .post(`http://localhost:${PORT}/api/notes`)
+   .send({title:"First Note", content:""})
+   .end((err,res) => {
+     expect(err).not.toBe(null);
+     expect(res.status).toEqual(400);
+     expect(res.text).toBe("Missing Content");
+   })
+ })
+});
+
+ describe('api', function(){
+   it('should contain a response body for a request made with a valid id', function(done){
+     request
+     .get(`http://localhost:${PORT}/api/notes?id=${idForGet}`)
+     .end((err, res => {
+       expect(res.status).toBe(200);
+       expect(res.body).not.toBe(null);
+       expect(note.id).toEqual(idForGet);
+       done();
+     }))
+   })
+ })
+
+ describe('api', function(){
+   it(' should respond with "not found" for an id that was not found', function(done){
+     request
+     .get(`http://localhost:${PORT}/api/notes?id=5`)
+     .end((err, res => {
+       expect(res.status).toBe(404);
+       expect(res.text).toEqual('invalid note');
+       done();
+     }))
+   })
+ })
+
  describe('api', function(){
    it('should return a status code of 404 for routes that have not been registered', function(done){
      request
-      .get(host + '/api/incorrect')
+      .get(`http://localhost:${PORT}/api/notregistered`)
       .end((err, res) => {
         expect(err).not.toBe(null);
-        expect(res.statu).toBe(404);
+        expect(res.status).toBe(404);
         done();
       });
-   })
- })
+   });
+ });
